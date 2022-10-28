5286635d138e3a11e98ea155366912726712a6f6
YaodongLiu <ttttttliu@qq.com>
2022-10-28 16:41:04 +0800

    初始化规则引擎项目

---
+++ src/main/java/org/example/felixlyd/ruleliteflow/RuleLiteflowApplication.java
@@ -0,0 +1,13 @@
+package org.example.felixlyd.ruleliteflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleLiteflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleLiteflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/felixlyd/ruleliteflow/RuleLiteflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.felixlyd.ruleliteflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleLiteflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
