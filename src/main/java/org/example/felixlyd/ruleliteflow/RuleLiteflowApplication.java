6e732c290b46271095608d4de5206d1163d0b97f
YaodongLiu <ttttttliu@qq.com>
2022-11-01 15:47:00 +0800

    增加eureka依赖

--- src/main/java/org/example/felixlyd/ruleliteflow/RuleLiteflowApplication.java
+++ src/main/java/org/example/felixlyd/ruleliteflow/RuleLiteflowApplication.java
@@ -2,8 +2,10 @@ package org.example.felixlyd.ruleliteflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RuleLiteflowApplication {
 
     public static void main(String[] args) {
